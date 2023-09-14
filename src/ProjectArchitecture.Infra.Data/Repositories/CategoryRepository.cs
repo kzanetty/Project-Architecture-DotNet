ac097d2b7b56e0e6283cad6ccbb5e31f5239dbc3
Henrique Zanetti <hzanettik12@gmail.com>
2023-09-13 22:14:02 -0300

    Creating category and product repositories

---
+++ src/ProjectArchitecture.Infra.Data/Repositories/CategoryRepository.cs
@@ -0,0 +1,48 @@
+﻿using ProjectArchitecture.Infra.Data.Context;
+using ProjectArchitecture.Domain.Interfaces;
+using ProjectArchitecture.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProjectArchitecture.Infra.Data.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly ApplicationDbContext _categoryContext;
+        public CategoryRepository(ApplicationDbContext context)
+        {
+            _categoryContext = context;
+        }
+
+        public async Task<Category> Create(Category category)
+        {
+            _categoryContext.Categories.Add(category);
+            await _categoryContext.SaveChangesAsync();
+            return category;
+        }
+
+        public async Task<IEnumerable<Category>> GetAllAsync()
+        {
+            return await _categoryContext.Categories.ToListAsync();
+        }
+
+        //Revisar o retorno desse método, tratamento de null
+        public async Task<Category?> GetByIdAsync(int? id)
+        {
+            return await _categoryContext.Categories.FindAsync(id);
+        }
+
+        public async Task<Category> Remove(Category category)
+        {
+            _categoryContext.Categories.Remove(category);
+            await _categoryContext.SaveChangesAsync();
+            return category;
+        }
+
+        public async Task<Category> Update(Category category)
+        {
+            _categoryContext.Categories.Update(category);
+            await _categoryContext.SaveChangesAsync();
+            return category;
+        }
+    }
+}
---
+++ src/ProjectArchitecture.Infra.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectArchitecture.Domain.Entities;
+using ProjectArchitecture.Domain.Interfaces;
+using ProjectArchitecture.Infra.Data.Context;
+
+namespace ProjectArchitecture.Infra.Data.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ApplicationDbContext _productContext;
+        public ProductRepository(ApplicationDbContext context)
+        {
+            _productContext = context;
+        }
+        public async Task<Product> CreateAsync(Product product)
+        {
+            _productContext.Products.Add(product);
+            await _productContext.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task<IEnumerable<Product>> GetAllAsync()
+        {
+            return await _productContext.Products.ToListAsync();
+        }
+
+        public async Task<Product?> GetByIdAsync(int? id)
+        {
+            return await _productContext.Products.FindAsync(id);
+        }
+
+        public async Task<Product?> GetProductCategoryAsync(int? id)
+        {
+            return await _productContext.Products.Include(c => c.Category).SingleOrDefaultAsync(p => p.Id == id);
+        }
+
+        public async Task<Product> RemoveAsync(Product product)
+        {
+            _productContext.Products.Remove(product);
+            await _productContext.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task<Product> UpdateAsync(Product product)
+        {
+            _productContext.Products.Update(product);
+            await _productContext.SaveChangesAsync();
+            return product;
+        }
+    }
+}
