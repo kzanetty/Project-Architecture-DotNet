bf5d021516905c2c8ab903914271c091a188e887
Henrique Zanetti <hzanettik12@gmail.com>
2023-09-15 23:01:36 -0300

    Creation of DTOs and implementation of AutoMapper

---
+++ src/ProjectArchitecture.Application/Mappings/DomainToDTOMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using ProjectArchitecture.Application.DTOs;
+using ProjectArchitecture.Domain.Entities;
+
+namespace ProjectArchitecture.Application.Mappings
+{
+    public class DomainToDTOMappingProfile : Profile
+    {
+        public DomainToDTOMappingProfile()
+        {
+            CreateMap<Category, CategoryDTO>().ReverseMap();
+            CreateMap<Product, ProductDTO>().ReverseMap();
+        }
+    }
+}
