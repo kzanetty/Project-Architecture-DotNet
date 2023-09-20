7097157eb80b8128caa7b67d78d2ff720ca1a690
Henrique Zanetti <hzanettik12@gmail.com>
2023-09-20 17:14:08 -0300

    Adding product and category controller

---
+++ src/ProjectArchitecture.WebApi/Controllers/CategoriesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectArchitecture.Application.Interfaces;
+
+namespace ProjectArchitecture.WebApi.Controllers
+{
+    [Route("categories")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCategories()
+        {
+            var categories = await _categoryService.GetCategories();
+            return Ok(categories);
+        }
+    }
+}
---
+++ src/ProjectArchitecture.WebApi/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectArchitecture.Application.Interfaces;
+
+namespace ProjectArchitecture.WebApi.Controllers
+{
+    [Route("products")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProducts()
+        {
+            var products = await _productService.GetProducts();
+            return Ok(products);
+        }
+    }
+}
